b24073608515a0267b420f4237d98c46cd586fa4
Norgerman <xyn0410@gmail.com>
2026-06-05 21:47:53 +0800

    chore(oxc): update config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         '.github/*',
         '**/build/**',
         '**/dist/**',
-        '**/bin/*',
+        '**/bin/**',
         '**/scripts/**',
         // website
         '**/.astro/**',
